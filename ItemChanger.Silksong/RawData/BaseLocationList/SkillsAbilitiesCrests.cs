7e436472a098b88a529c0ac6c2c9c9734a829bdc
Thomas Lunn <thomas.lunn5@gmail.com>
2026-04-11 22:59:10 +0100

    Move data definitions into existing file

--- ItemChanger.Silksong/RawData/BaseLocationList/SkillsAbilitiesCrests.cs
+++ ItemChanger.Silksong/RawData/BaseLocationList/SkillsAbilitiesCrests.cs
@@ -1,11 +1,35 @@
 using ItemChanger.Locations;
 using ItemChanger.Silksong.Locations;
 using Benchwarp.Data;
+using ItemChanger.Silksong.Containers;
+using ItemChanger.Silksong.Tags;
+using ItemChanger.Tags;
 
 namespace ItemChanger.Silksong.RawData;
 
 internal static partial class BaseLocationList
 {
+    private static Location CreateWeaverCorpseLocation(
+        string name,
+        string sceneName,
+        string logicObjectName = "Shrine Weaver Ability",
+        string spriteObjectPath = "Ability Scene/Burst Deactivate"
+    )
+    {
+        return new ObjectLocation()
+        {
+            Name = name,
+            SceneName = sceneName,
+            ObjectName = logicObjectName,
+            Correction = default,
+            Tags =
+            [
+                new OriginalContainerTag() { ContainerType = ContainerNames.WeaverCorpse },
+                new DestroyOnContainerReplaceTag() { ObjectPath = spriteObjectPath },
+            ]
+        };
+    }
+    
     public static Location Crest_of_Wanderer => new CrestCorpseLocation
     {
         SceneName = SceneNames.Chapel_Wanderer,
@@ -47,4 +71,51 @@ internal static partial class BaseLocationList
         SceneName = SceneNames.Weave_10,
         Name = LocationNames.Eva,
     };
+    
+    public static Location Silkspear => CreateWeaverCorpseLocation(
+        LocationNames.Silkspear,
+        SceneNames.Mosstown_02
+    );
+
+    public static Location Thread_Storm => CreateWeaverCorpseLocation(
+        LocationNames.Thread_Storm,
+        SceneNames.Greymoor_22
+    );
+
+    public static Location Sharpdart => CreateWeaverCorpseLocation(
+        LocationNames.Sharpdart,
+        SceneNames.Crawl_05,
+        spriteObjectPath: "Ability Scene (2)/Burst Deactivate"
+    );
+
+    public static Location Swift_Step => CreateWeaverCorpseLocation(
+        LocationNames.Swift_Step,
+        SceneNames.Bone_East_05,
+        spriteObjectPath: "Ability Scene (1)/Burst Deactivate"
+    );
+    
+    public static Location Cling_Grip => CreateWeaverCorpseLocation(
+        LocationNames.Cling_Grip,
+        SceneNames.Shellwood_10,
+        spriteObjectPath: "Ability Scene (1)/Burst Deactivate"
+    );
+
+    public static Location Clawline => CreateWeaverCorpseLocation(
+        LocationNames.Clawline,
+        SceneNames.Under_18,
+        spriteObjectPath: "Ability Scene/Burst Deactivate/Scenery"
+    );
+
+    public static Location Silk_Soar => CreateWeaverCorpseLocation(
+        LocationNames.Silk_Soar,
+        SceneNames.Abyss_08,
+        spriteObjectPath: "weaver_spire_base control/Burst Deactivate"
+    ).WithTag(
+        new RaiseFsmEventOnGiveTag()
+        {
+            SceneName =  SceneNames.Abyss_08,
+            Event = "SHRINE SEQUENCE END",
+            ObjectPath = "weaver_spire_base control"
+        }
+    );
 }
